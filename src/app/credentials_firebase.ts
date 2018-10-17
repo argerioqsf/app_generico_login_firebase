9eaf092a02cef873cf3f112ad10401846917d6ca
argerioqsf <39819614+argerioqsf@users.noreply.github.com>
2018-10-17 18:17:10 -0300

    Delete credentials_firebase.ts

--- src/app/credentials_firebase.ts
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_CREDENTIALS = {
-    apiKey: "AIzaSyBGD14jOaYyhY4qyUR_fuE--nNZzD-h8PU",
-    authDomain: "app-meta-d0e38.firebaseapp.com",
-    databaseURL: "https://app-meta-d0e38.firebaseio.com",
-    projectId: "app-meta-d0e38",
-    storageBucket: "app-meta-d0e38.appspot.com",
-    messagingSenderId: "814301693682"
-};
